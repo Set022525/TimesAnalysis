9421df2d1c74e52e5b7a49d2865c485048946cfe
みなふ <yamate-hachioji@outlook.jp>
2021-04-05 00:59:41 +0900

    MailApp

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-const properties = PropertiesService.getScriptProperties()
+const properties = PropertiesService.getScriptProperties();
 const slack_app_token = properties.getProperty("SLACKAPPTOKEN") as string;
-
 
 // function postDM() {
 //   const message = "あ";
@@ -30,7 +29,6 @@ const slack_app_token = properties.getProperty("SLACKAPPTOKEN") as string;
 //  * @return {string} チャンネルID
 //  */
 
-
 // function getChannelID_(member_id) {
 //   const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
 //     method: "post",
@@ -51,35 +49,42 @@ const slack_app_token = properties.getProperty("SLACKAPPTOKEN") as string;
 // }
 
 function normalizeRequest_(e) {
-    //  report(e)
-    let postData = e.postData
-    let request = postData.contents || postData.getDataAsString()
-    return JSON.parse(request)
-  }
-
-  function createTextOutput_(text) {
-    return ContentService.createTextOutput(text).setMimeType(ContentService.MimeType.TEXT)
-  }
-
-  ​function isValidEvent(event) {
-    return !event.hidden;
-  }
-
-function doPost(e) {
-    let REQUEST_TYPE_URL_VERIFICATION = 'url_verification'
-    let REQUEST_TYPE_EVENT_CALLBACK = 'event_callback'
-  ​
-    let request = normalizeRequest_(e)
-    let requestType = request.type
-    //  report(request)
-    if (requestType === REQUEST_TYPE_URL_VERIFICATION) {
-      let challenge = request.challenge
-      return createTextOutput_(challenge)
-    } else if (requestType === REQUEST_TYPE_EVENT_CALLBACK && isValidEvent(request.event)) {
-      let event = request.event;
-      let user = event.user 
-    }
+  //  report(e)
+  let postData = e.postData;
+  let request = postData.contents || postData.getDataAsString();
+  return JSON.parse(request);
 }
 
+function createTextOutput_(text) {
+  return ContentService.createTextOutput(text).setMimeType(
+    ContentService.MimeType.TEXT
+  );
+}
 
+function isValidEvent(event) {
+  return !event.hidden;
+}
 
+function doPost(e) {
+  let REQUEST_TYPE_URL_VERIFICATION = "url_verification";
+  let REQUEST_TYPE_EVENT_CALLBACK = "event_callback";
+  let request = normalizeRequest_(e);
+  let requestType = request.type;
+  //  report(request)
+  if (requestType === REQUEST_TYPE_URL_VERIFICATION) {
+    let challenge = request.challenge;
+    return createTextOutput_(challenge);
+  } else if (
+    requestType === REQUEST_TYPE_EVENT_CALLBACK &&
+    isValidEvent(request.event)
+  ) {
+    let event = request.event;
+    let user = event.user;
+
+    let param: GoogleAppsScript.Mail.MailAdvancedParameters;
+    param.to = "yukikaze.0511@gmail.com";
+    param.body = user;
+
+    MailApp.sendEmail(param);
+  }
+}
