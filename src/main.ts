6a828e624d84298d53c6a6153c843149dce9f0c3
Set022525 <set02kagerou@gmail.com>
2021-04-05 22:36:22 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,100 @@
 const properties = PropertiesService.getScriptProperties();
 const slack_app_token = properties.getProperty("SLACKAPPTOKEN") as string;
+let user;
 
-// function postDM() {
-//   const message = "あ";
-//   //【処理1】DMを開き、チャンネルIDを取得する
-//   const member_id = ; //メンバーIDを指定
-//   const channel_id = getChannelID_(member_id);
 
-//   //【処理2】指定の[チャンネルID]にDMを送信する
-//   const message_options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-//     method: "post",
-//     contentType: "application/x-www-form-urlencoded",
-//     payload: {
-//       token: slack_app_token,
-//       channel: channel_id,
-//       text: message,
-//     },
-//   };
+var ss = SpreadsheetApp.getActiveSpreadsheet();
+var sh = ss.getActiveSheet();
 
-//   const message_url = "https://slack.com/api/chat.postMessage";
-//   UrlFetchApp.fetch(message_url, message_options);
-// }
+function getSlack() {
 
-// /**
-//  * メンバーIDを受け取りチャンネルIDを返す
-//  *
-//  * @param {string} メンバーID
-//  * @return {string} チャンネルID
-//  */
+  // チャンネルリストを取得
+  var token = "取得したトークン";
+  var url = "https://slack.com/api/channels.list?token="+token+"";
+  var response = UrlFetchApp.fetch(url);
+  var json = response.getContentText();
+  var data = JSON.parse(json);
 
-// function getChannelID_(member_id) {
-//   const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-//     method: "post",
-//     contentType: "application/x-www-form-urlencoded",
-//     payload: {
-//       token: slack_app_token,
-//       users: member_id,
-//     },
-//   };
 
-//   //必要scope = im:write
-//   const url = "https://slack.com/api/conversations.open";
-//   const response = UrlFetchApp.fetch(url, options);
-//   const obj = JSON.parse(response);
-//   console.log(obj);
+  console.log(response,json,data)
+// 対象となるチャンネル名から対応するチャンネルIDを探し出す
+//   var channel_name = sh.getRange(1,2).getValue();
+//   for (var i=0; i<data.channels.length; i++) {
+//     if (data.channels[i].name == channel_name) {
+//       var channel_id = data.channels[i].id;
+//       break;
+//     }
+//   }
 
-//   return obj.channel.id;
+//   // 取得したチャンネルIDをもとにチャンネル内のすべてのメッセージを取得
+//   var url = "https://slack.com/api/channels.history?token="+token+"&channel="+channel_id+"";
+//   var response = UrlFetchApp.fetch(url);
+//   var json = response.getContentText();
+//   var data = JSON.parse(json);
+
+//   // スプレッドシートに取り込む
+
+//   // 最終行取得
+//   var lr = sh.getLastRow();
+//   // 初期化
+//   if (lr > 2) sh.getRange(3,1,lr-2,sh.getLastColumn()).clear();
+//   // 出てきそうな項目名を予め列挙しておく(この辺は必要に応じて加減すればよいでしょう）
+//   var item = [
+//     "file",
+//     "text",
+//     "type",
+//     "user",
+//     "ts"
+//   ];
+//   // 2次元配列化
+//   var ary = [];
+//   var ary2 = [];
+//   for (var i=0; i<data.messages.length; i++) {
+//     for (var j=0; j<item.length; j++) {
+//       ary.push(data.messages[i][item[j]]);
+//       // もしundefinedなら空欄に直す
+//       if (!ary[j]) {
+//         ary[j] = "";
+//       }
+//     }
+//     ary2.push(ary);
+//     ary = [];
+//   }
+//   // スプレッドシートに転記
+//   sh.getRange(3,1,ary2.length,item.length).setValues(ary2);
+}
+
+
+
+
+
+
+
+
+// let ss = SpreadsheetApp.getActiveSpreadsheet();
+// let sh = ss.getActiveSheet();
+
+// function getChannelHistory() {
+
+//   var token = slack_app_token;
+//   var url = "https://slack.com/api/conversations.history="+token+"";
+//   var response = UrlFetchApp.fetch(url);
+//   var json = response.getContentText();
+//   var data = JSON.parse(json);
+
+//   var channel_name = sh.getRange(1,2).getValue();
+//   for (var i=0; i<data.channels.length; i++) {
+//     if (data.channels[i].name == channel_name) {
+//       var channel_id = data.channels[i].id;
+//       break;
+//     }
+//   }
+//   let options = 
+//   var url = "https://slack.com/api/channels.history?token="+token+"&channel="+channel_id+"";
+//   var response = UrlFetchApp.fetch(url,options);
+//   var json = response.getContentText();
+//   var data = JSON.parse(json);
+//   return data
 // }
 
 function normalizeRequest_(e) {
@@ -108,3 +157,25 @@ function report(...data: any) {
 function testReport() {
   console.log(report(1, 2, 3, ["abc"], { def: 4 }));
 }
+
+function postDM() {
+  
+  const message = "あ";
+  //【処理1】DMを開き、チャンネルIDを取得する
+  const member_id = user; //メンバーIDを指定
+  
+  //【処理2】指定の[チャンネルID]にDMを送信する
+  const message_options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: "post",
+    contentType: "application/x-www-form-urlencoded",
+    payload: {
+      token: slack_app_token,
+      channel: member_id,
+      text: message,
+    },
+  };
+
+  const message_url = "https://slack.com/api/chat.postMessage";
+  UrlFetchApp.fetch(message_url, message_options);
+}
+
